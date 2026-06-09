629131d658406cf2eb0f7a3edb0f0cbd653d08d2
Shaj2311 <haideralijafri2007@gmail.com>
2026-06-09 14:40:52 +0500

    Add art width and height constants, switch art to char arrays

--- art.h
+++ art.h
@@ -1,7 +1,10 @@
 #ifndef ART_H
 #define ART_H
 
-#define A_ART "a"
-#define B_ART "b"
+#define ART_WIDTH 8
+#define ART_HEIGHT 5
+
+const char A_ART[] = {'a', 0};
+const char B_ART[] = {'b', 0};
 
 #endif

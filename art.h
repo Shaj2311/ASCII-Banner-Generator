7ffaa3234399356d9ccc9f37c571b77a39b738f2
Shaj2311 <haideralijafri2007@gmail.com>
2026-06-09 15:06:53 +0500

    Complete letter A art, create constants for (almost) all ascii chars

--- art.h
+++ art.h
@@ -4,7 +4,46 @@
 #define ART_WIDTH 8
 #define ART_HEIGHT 5
 
-const char A_ART[] = {'a', 0};
-const char B_ART[] = {'b', 0};
+// SOLID BLOCK
+#define b (char)219
+
+//  __________________________
+// /\                         \
+// \_| w: top-right corner    |
+//   | x: bottom-right corner |
+//   | y: bottom-left corner  |
+//   | z: top-left corner     |
+//   |                        |
+//   |           z|w          |
+//   |           -+-          |
+//   |           y|x          |
+//   |   _____________________|_
+//    \_/_______________________/
+#define w (char)187
+#define x (char)188
+#define y (char)200
+#define z (char)201
+
+
+// double horizontal and vertical lines
+#define h (char)205
+#define v (char)186
+
+// space and new line
+const char s = ' ';
+const char n = '\n';
+
+
+
+
+const char A_ART[] =
+{
+	s,b,b,b,b,b,w,s,n,
+	b,b,z,h,h,b,b,w,n,
+	b,b,b,b,b,b,b,v,n,
+	b,b,z,h,h,b,b,v,n,
+	b,b,v,s,s,b,b,v,n,
+	y,h,x,s,s,y,h,x,0
+};
 
 #endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "art.h"
+#include <stdio.h>
+int main()
+{
+	printf("%s", A_ART);
+	printf("\033[%dA", ART_HEIGHT);
+}

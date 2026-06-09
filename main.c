af6dc2596b0a24b81d69f8e56285f40b3db4f0b0
Shaj2311 <haideralijafri2007@gmail.com>
2026-06-09 23:13:02 +0500

    Complete printArt function

--- main.c
+++ main.c
@@ -1,17 +1,67 @@
 #include "art.c"
 #include <stdio.h>
 
-int main()
+int printArt(char *str, const char *arts[])
 {
-	//printf("%s", A_ART);
-	//printf("\033[%dA", ART_HEIGHT);
+	//validate string
+	char *ptr = str;
+	ptr--;
+	while(*(++ptr))
+	{
+		if(*ptr >= 'a' && *ptr <= 'z')
+			continue;
+		if(*ptr >= 'A' && *ptr <= 'Z')
+			continue;
+		if(*ptr == ' ')
+			continue;
+
+		return 0;
+	}
+
 	//clear screen
-	//IDEA: instead of clearing screen, run a ART_HEIGHT
-	//puts("") loop and come back up? To ensure that there's
-	// enough space down there?
 	printf("\033[H\033[2J");
 
-	printf("%s%s%s%s%s%s%s%s%s%s%s", A_ART, U_ART, N_ART, SPACE_ART, I_ART, S_ART, SPACE_ART, G_ART, O_ART, N_ART, E_ART);
+
+	ptr = str;
+	while(*ptr)
+	{
+		if(*ptr >= 'a' && *ptr <= 'z')
+			printf("%s", arts[*ptr - 'a']);
+		if(*ptr >= 'A' && *ptr <= 'Z')
+			printf("%s", arts[*ptr - 'A']);
+		if(*ptr == ' ')
+			printf("%s", SPACE_ART);
+		ptr++;
+	}
+
+
+
+
+
 	for(int i = 0; i < ART_HEIGHT; i++)
 		puts("");
+
+	return 1;
+}
+
+int main()
+{
+	const char *alphabetArts[] =
+	{
+		A_ART, B_ART, C_ART, D_ART,
+		E_ART, F_ART, G_ART, H_ART,
+		I_ART, J_ART, K_ART, L_ART,
+		M_ART, N_ART, O_ART, P_ART,
+		Q_ART, R_ART, S_ART, T_ART,
+		U_ART, V_ART, W_ART, X_ART,
+		Y_ART, Z_ART
+	};
+
+	char *str = "Aun is Gone";
+	if(!printArt(str, alphabetArts))
+	{
+		printf("Alphabets and spaces only please UwU");
+		return 1;
+	}
+	return 0;
 }
